688e79598051d50dc0c151abc2e93e329c85e58d
Benjamin Wilberforce <bwamonoo@st.ug.edu.gh>
2026-02-12 09:25:05 +0000

    Task: Switch "States" based on what is sensed:
    State 1: Monitoring (Normal)
    
    State 2: Alert (Emergency)

---
+++ lab3.py
@@ -0,0 +1,31 @@
+import asyncio
+import random
+from spade.agent import Agent
+from spade.behaviour import CyclicBehaviour
+
+class ReactiveBehaviour(CyclicBehaviour):
+    async def run(self):
+        # PERCEPTION
+        heat_level = random.randint(20, 100)
+        print(f"Current Heat: {heat_level}°C")
+        
+        # REACTION (State Transition)
+        if heat_level > 80:
+            print(">>> STATE: EMERGENCY! (Heat Critical)")
+        else:
+            print(">>> STATE: NORMAL (Monitoring)")
+            
+        await asyncio.sleep(2)
+
+class Lab3Agent(Agent):
+    async def setup(self):
+        self.add_behaviour(ReactiveBehaviour())
+
+async def main():
+    agent = Lab3Agent("your_username@xmpp.jp", "your_password")
+    await agent.start()
+    await asyncio.sleep(10)
+    await agent.stop()
+
+if __name__ == "__main__":
+    asyncio.run(main())
